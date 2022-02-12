1c1a17b5f7b267dffc14f2ba10c39c68cc46b5ab
Mr.Newbie <atharafiaffandy@gmail.com>
2022-02-12 15:52:10 +0700

    sticky-effect

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const aboutUs = document.getElementById("about-us");
 const navbarSticky = document.getElementsByClassName("container-navbar");
-var aboutUsCounted = aboutUs.clientHeight - 100;
 var height = window.clientY;
 const aboutUsButton = document.getElementsByClassName("info-button");
 $(".info-button").on("click", function (e) {
   e.preventDefault();
-  $("html, body").animate({ scrollTop: $(aboutUs).offset().top }, 800);
+  $("html, body").animate({ scrollTop: $(aboutUs).offset().top }, 500);
 });
 document.addEventListener("scroll", (e) => {
-  if (window.pageYOffset >= 60) {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 90) {
+    navbarSticky[0].classList.add("scrolled-navbar");
+  }
+  if (window.pageYOffset <= 89) {
+    navbarSticky[0].classList.remove("scrolled-navbar");
+    console.log("hello");
   }
 });
