d1a837013917b9add1fa04f5cfa732d4a900b2a0
Mr.Newbie <atharafiaffandy@gmail.com>
2022-02-12 14:46:37 +0700

    JQuery

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const navbarSticky = document.getElementsByClassName("container-navbar");
 var aboutUsCounted = aboutUs.clientHeight - 100;
 var height = window.clientY;
 const aboutUsButton = document.getElementsByClassName("info-button");
-aboutUsButton[0].addEventListener("click", () => {
-  window.scrollTo(0, aboutUsCounted);
+$(".info-button").on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: $(aboutUs).offset().top }, 800);
 });
 document.addEventListener("scroll", (e) => {
   if (window.pageYOffset >= 60) {
