5b27d3f000d915c6470755e2e98e72dc478e140e
Mr.Newbie <atharafiaffandy@gmail.com>
2022-02-12 22:15:42 +0700

    detailing

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const aboutUs = document.getElementById("about-us");
-const navbarSticky = document.getElementsByClassName("container-navbar");
-var height = window.clientY;
-const aboutUsButton = document.getElementsByClassName("info-button");
-$(".info-button").on("click", function (e) {
+const navbarItem = document.querySelectorAll(".container-navbar ul li a");
+const navbarSticky = document.querySelectorAll(".container-navbar");
+$(".info-button, .about-us-button").on("click", function (e) {
   e.preventDefault();
-  $("html, body").animate({ scrollTop: $(aboutUs).offset().top }, 500);
+  $("html, body").animate({ scrollTop: $(aboutUs).offset().top - 100 }, 500);
 });
-document.addEventListener("scroll", (e) => {
-  console.log(window.pageYOffset);
+$(".home-button").on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: $("header").offset().top }, 500);
+});
+document.addEventListener("scroll", () => {
   if (window.pageYOffset >= 90) {
     navbarSticky[0].classList.add("scrolled-navbar");
+    navbarSticky[0].classList.add("animated-navbar");
+    if (window.pageYOffset > aboutUs.clientHeight) {
+      navbarItem.forEach((e) => {
+        e.classList.add("blacktext");
+      });
+    } else {
+      navbarItem.forEach((e) => {
+        e.classList.remove("blacktext");
+      });
+    }
   }
   if (window.pageYOffset <= 89) {
     navbarSticky[0].classList.remove("scrolled-navbar");
-    console.log("hello");
+    navbarSticky[0].classList.remove("animated-navbar");
   }
 });
